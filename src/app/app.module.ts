b5d0159f7133c203d80abbab4e0aae27f77bf05d
Marcos-111 <escalzomd@gmail.com>
2021-04-27 15:32:58 -0300

    basic footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent,],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent,],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],

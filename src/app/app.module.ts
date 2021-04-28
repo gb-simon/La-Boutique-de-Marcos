2354554202cab2cc5c1a482b52ac0ec730f8976e
Marcos-111 <escalzomd@gmail.com>
2021-04-28 13:37:45 -0300

    header fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-<<<<<<< HEAD
 import { FooterComponent } from './footer/footer.component';
 import { CommerceComponent } from './commerce/commerce.component';
 import { FiltersComponent } from './filters/filters.component';
-
-@NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, CommerceComponent, FiltersComponent,],
-  imports: [BrowserModule, NgbModule],
-=======
-import { FooterComponent } from "./footer/footer.component";
-import { CommerceComponent } from "./commerce/commerce.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
@@ -25,9 +19,9 @@ import { FormsModule } from "@angular/forms";
     HomeComponent,
     FooterComponent,
     CommerceComponent,
+    FiltersComponent,
   ],
   imports: [BrowserModule, FormsModule, NgbModule, HttpClientModule],
->>>>>>> 53f8d68603f2d9b921ebab9b0a4fe783c2333825
   providers: [],
   bootstrap: [AppComponent],
 })

53f8d68603f2d9b921ebab9b0a4fe783c2333825
gonzalo <aguilarsimon01@gmail.com>
2021-04-28 12:32:42 -0300

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+import client_id from ".env"
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.

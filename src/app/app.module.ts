6e4c513e8c988d7d783bbc9952da892c3f249005
gonzalo <aguilarsimon01@gmail.com>
2021-04-28 12:17:26 -0300

    I added a search option connected w/ the Unsplash API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { FooterComponent } from './footer/footer.component';
-import { CommerceComponent } from './commerce/commerce.component';
+import { FooterComponent } from "./footer/footer.component";
+import { CommerceComponent } from "./commerce/commerce.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, CommerceComponent,],
-  imports: [BrowserModule, NgbModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CommerceComponent,
+  ],
+  imports: [BrowserModule, FormsModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  client_id: "ZrFuDKyLgJ4ZUMHdVzGTQWkA3qjU9E_vrVAS5IDGpio"
 };
 
 /*

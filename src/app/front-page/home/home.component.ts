22afdcc136ccbe3c9cecb644d0cb12d88c3de06f
gonzalo <aguilarsimon01@gmail.com>
2021-04-30 13:41:54 -0300

    I'm modifying the commerce section

--- src/app/front-page/home/home.component.ts
+++ src/app/front-page/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -15,12 +16,12 @@ export class HomeComponent {
   public data = [];
   public url = `https://api.unsplash.com/search/photos?client_id=${environment.client_id}&query=`;
   trends = "radio, phones, television, coffee";
-  
-  constructor(private http: HttpClient) {}
 
+  constructor(private http: HttpClient) {}
 
   searchImage() {
     this.http.get(this.url + this.searchKeyword).subscribe((res) => {
       this.data = res["results"];
     });
-  }}
+  }
+}
--- src/app/front-page/product/product.component.ts
+++ src/app/front-page/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-product',
@@ -7,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  public searchKeyword = "";
+  public data = [];
+  public url = `https://api.unsplash.com/search/photos?client_id=${environment.client_id}&query=`;
+  trends = "radio, phones, television, coffee";
 
+  constructor(private http: HttpClient) {}
+
+  searchImage() {
+    this.http.get(this.url + this.searchKeyword).subscribe((res) => {
+      this.data = res["results"];
+    });
+  }
   ngOnInit(): void {
   }
 

42e1354e622e85f8f0ca47d313cccceba7b719fd
gonzalo <aguilarsimon01@gmail.com>
2021-05-05 09:46:17 -0300

    I added the login & register sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { FrontComponent } from './front-page/front.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: FrontComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: '',   redirectTo: '', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
